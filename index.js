036ce3151ff00b06b27e7d097ea7f0ece39029d1
Anna Zocher <anna.zocher@gmail.com>
2021-06-02 15:49:11 -0700

    finalized example of OMDB API call

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
 const express = require('express')
 // import axios
 const axios = require('axios')
+// import and config dotenv library
+require('dotenv').config()
 // define port 
 const PORT = 3000
+// define API key var
+const omdbApiKey = process.env.OMDB_API_KEY
 
 // declare an instance of app
 const app = express()
 
 // create a home route
 app.get('/', (req, res) => {
-    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=')
+    axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${omdbApiKey}`)
         .then((resFromAPI) => {
-            console.log(resFromAPI.data.Production)
-            res.send(resFromAPI.data.Production)
+            console.log(resFromAPI.data.Title)
+            res.send(resFromAPI.data.Title)
         })
         .catch(err => {console.log(err)})
 })
